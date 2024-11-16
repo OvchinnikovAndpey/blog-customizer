a3b38f0879bffeb0aed29b2f3e35755a5fe6b35f
Andrey Ovchinnikov <andpey.ovchinnikov@yandex.ru>
2024-11-16 19:38:24 +0300

    пробный комит в ветку review. Проект почти готов

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,142 @@
+import clsx from 'clsx';
+import { useRef, useState, useEffect } from 'react';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	style: ArticleStateType;
+	setStyle: (style: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	style,
+	setStyle,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [currentStyle, setCurrentStyle] = useState(style);
+
+	const formRef = useRef<HTMLFormElement>(null);
+
+	// function formOpenHandler() {
+	// 	setIsOpen(true);
+	// }
+
+	function formSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		setStyle(currentStyle);
+		setIsOpen(false);
+	}
+
+	function formResetHandler() {
+		setCurrentStyle(defaultArticleState);
+		setStyle(defaultArticleState);
+	}
+
+	useEffect(() => {
+		function closeEsc(event: KeyboardEvent) {
+			if (event.key === 'Escape') {
+				setIsOpen(false);
+			}
+		}
+		document.addEventListener('keydown', closeEsc);
+		return () => {
+			document.removeEventListener('keydown', closeEsc);
+		};
+	}, [isOpen]);
+
+	function handleFontFamilyChange(item: OptionType) {
+		setCurrentStyle({ ...currentStyle, fontFamilyOption: item });
+	}
+
+	function handlerFontColorChange(item: OptionType) {
+		setCurrentStyle({ ...currentStyle, fontColor: item });
+	}
+
+	function handlerBackgroundChange(item: OptionType) {
+		setCurrentStyle({ ...currentStyle, backgroundColor: item });
+	}
+	function handlerContentWidthChange(item: OptionType) {
+		setCurrentStyle({ ...currentStyle, contentWidth: item });
+	}
+
+	function handlerFontSizeChange(item: OptionType) {
+		setCurrentStyle({ ...currentStyle, fontSizeOption: item });
+	}
+
+	// Функция для варианта попроще, не через кастомные хуки
+	// function handleChange(key: keyof ArticleStateType, item: OptionType) {
+	// 	setCurrentStyle({ ...currentStyle, [key]: item });
+	// }
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form
+					ref={formRef}
+					onSubmit={formSubmitHandler}
+					onReset={formResetHandler}
+					className={styles.form}>
+					<Text
+						as='h2'
+						size={31}
+						weight={800}
+						family={'open-sans'}
+						uppercase
+						dynamicLite>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={currentStyle.fontFamilyOption}
+						onChange={handleFontFamilyChange}
+					/>
+					<RadioGroup
+						name='font size'
+						title='размер шрифта'
+						options={fontSizeOptions}
+						selected={currentStyle.fontSizeOption}
+						onChange={handlerFontSizeChange}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={currentStyle.fontColor}
+						onChange={handlerFontColorChange}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={currentStyle.backgroundColor}
+						onChange={handlerBackgroundChange}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={currentStyle.contentWidth}
+						onChange={handlerContentWidthChange}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
